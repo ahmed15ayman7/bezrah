a02e858d18ad2a83944d30ab960e5208b7fc3276
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-12-03 23:08:48 +0200

    bezrah

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   return (
@@ -8,40 +8,60 @@ const Header = () => {
         {/* Logo */}
         <Link href="/" className="flex items-center space-x-2">
           {/* Logo Image using Next.js Image component */}
-          <Image 
-            src="/images/logg.png" 
-            alt="Green Roof Solutions Logo" 
+          <Image
+            src="/images/logg.png"
+            alt="Green Roof Solutions Logo"
             width={44} // specify width
             height={44} // specify height
-            className="object-contain pt-2" 
+            className="object-contain pt-2"
           />
-          <Image 
-            src="/images/remin.png" 
-            alt="Green Roof Solutions Logo" 
+          <Image
+            src="/images/remin.png"
+            alt="Green Roof Solutions Logo"
             width={60} // specify width
             height={60} // specify height
-            className="object-contain pt-2" 
+            className="object-contain pt-2"
           />
         </Link>
 
         {/* Navigation Links */}
         <ul className="flex space-x-8 text-lg">
           <li>
-            <Link href="/" className="text-white hover:text-gray-400 transition-all">Home</Link>
+            <Link
+              href="/"
+              className="text-white hover:text-gray-400 transition-all"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/about" className="text-white hover:text-gray-400 transition-all">About</Link>
+            <Link
+              href="/about"
+              className="text-white hover:text-gray-400 transition-all"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link href="/services" className="text-white hover:text-gray-400 transition-all">Services</Link>
+            <Link
+              href="/services"
+              className="text-white hover:text-gray-400 transition-all"
+            >
+              Services
+            </Link>
           </li>
           <li>
-            <Link href="/contact" className="text-white hover:text-gray-400 transition-all">Contact</Link>
+            <Link
+              href="/contact"
+              className="text-white hover:text-gray-400 transition-all"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
