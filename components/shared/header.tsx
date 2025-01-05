4ba9fcf9a0d68719a6ac4a6908a32a2a2a39b2e2
bezrahweb <bezrahweb@gmail.com>
2025-01-05 16:45:23 +0200

    Update header.tsx

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
       <AppBar
         position="fixed"
         sx={{
-          background: "linear-gradient(to right, #013820, #013820)",
+          background: "linear-gradient(to right, #013820, #4ebc78)",
           color: "white",
           zIndex: (theme) => theme.zIndex.drawer + 1,
         }}
