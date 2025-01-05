14c4720e4ac512d5f1452cda5a77af32fc3735d6
bezrahweb <bezrahweb@gmail.com>
2025-01-05 19:51:57 +0200

    Update header.tsx

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -102,7 +102,7 @@ const Header = () => {
         <Box
           sx={{
             width: 250,
-            background: "linear-gradient(to right, #4ebc78, #013820)",
+            background: "linear-gradient(to right, #013820, #4ebc78)",
             color: "white",
             height: "100%",
           }}
